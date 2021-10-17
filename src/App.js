ef9dd398cc361d31d9e783bfdc2b509b035bb1fe
Salvatore Dibenedetto <salvatore.dibenedetto@eidosmedia.com>
2021-10-17 17:28:19 +0200

    setup project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import logo from './assets/logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className={'app'}>
+      <header className={'app-header'}>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className={'logo-link'}
+          href={'https://www.datahow.ch/'}
+          target={'_blank'}
+          rel={'noopener noreferrer'}
         >
-          Learn React
+          <img src={logo} className={'app-logo'} alt={'logo'} />
         </a>
       </header>
+      <div className={'wrapper'}>
+          <p>grafico</p>
+      </div>
+      <footer>
+          <p>COVID-19 pandemic progression monitor</p>
+      </footer>
     </div>
   );
 }
